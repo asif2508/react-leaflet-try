4ed403b25f1e38626e6720a02ba4cf391a2bf6d6
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-30 00:33:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MapTry from './components/MapTry';
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is how it looks!</h1>
+      <p>We can do many more things with this.</p>
+      <MapTry></MapTry>
     </div>
   );
 }
---
+++ src/components/MapTry.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { TileLayer } from 'react-leaflet/TileLayer';
+import { useMap } from 'react-leaflet/hooks';
+import { Marker, Popup } from 'react-leaflet';
+const MapTry = () => {
+  const position = [51.505, -0.09]
+  return (
+    <div style={{ height: "500px", margin:"50px" }}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{ height: "100%", minHeight: "100%" }} >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapTry;
