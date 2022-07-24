1d1d8cb78aeeba8c70597bc39b34898acc4f74d8
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-07-24 21:34:06 +0600

    automatic location added

--- src/components/MapTry.js
+++ src/components/MapTry.js
@@ -1,22 +1,54 @@
-import React from 'react';
-import { MapContainer } from 'react-leaflet/MapContainer';
-import { TileLayer } from 'react-leaflet/TileLayer';
-import { Marker, Popup } from 'react-leaflet';
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+import icon from "./Constant";
 const MapTry = () => {
-  const position = [51.505, -0.09]
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const [bbox, setBbox] = useState([]);
+
+    const map = useMap();
+
+    useEffect(() => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius);
+        circle.addTo(map);
+        setBbox(e.bounds.toBBoxString().split(","));
+      });
+    }, [map]);
+
+    return position === null ? null : (
+      <Marker position={position} icon={icon}>
+        <Popup>
+          You are here. <br />
+          Map bbox: <br />
+          <b>Southwest lng</b>: {bbox[0]} <br />
+          <b>Southwest lat</b>: {bbox[1]} <br />
+          <b>Northeast lng</b>: {bbox[2]} <br />
+          <b>Northeast lat</b>: {bbox[3]}
+        </Popup>
+      </Marker>
+    );
+  }
   return (
     <div style={{ height: "500px", margin:"50px" }}>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{ height: "100%", minHeight: "100%" }} >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <MapContainer
+      center={[49.1951, 16.6068]}
+      zoom={13}
+      scrollWheelZoom
+      style={{ height: "100vh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker />
+    </MapContainer>
     </div>
   );
 };
