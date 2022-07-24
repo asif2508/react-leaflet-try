1fdb1b2a2a4313ca4135f8197a58ef932b7aac0d
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-07-24 22:52:52 +0600

    draggable marker added

--- src/components/MapTry.js
+++ src/components/MapTry.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
@@ -6,11 +6,26 @@ import L from "leaflet";
 import icon from "./Constant";
 const MapTry = () => {
   function LocationMarker() {
+    const [draggable, setDraggable] = useState(false)
     const [position, setPosition] = useState(null);
+    
     const [bbox, setBbox] = useState([]);
-
     const map = useMap();
-
+    const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        if (marker != null) {
+          setPosition(marker.getLatLng())
+        }
+      },
+    }),
+    [],
+  )
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d)
+  }, [])
     useEffect(() => {
       map.locate().on("locationfound", function (e) {
         setPosition(e.latlng);
@@ -23,20 +38,32 @@ const MapTry = () => {
     }, [map]);
 
     return position === null ? null : (
-      <Marker position={position} icon={icon}>
+      <Marker 
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef} 
+      icon={icon}>
         <Popup>
-          You are here. <br />
-          Map bbox: <br />
-          <b>Southwest lng</b>: {bbox[0]} <br />
-          <b>Southwest lat</b>: {bbox[1]} <br />
-          <b>Northeast lng</b>: {bbox[2]} <br />
-          <b>Northeast lat</b>: {bbox[3]}
+          <span onClick={toggleDraggable}>
+          {draggable
+            ? <>
+            <p>Marker is draggable Now</p>
+            </>
+            : 
+            <>
+            <p>Click here to make marker draggable</p>
+            </>
+            
+            }
+        </span>
         </Popup>
       </Marker>
     );
   }
   return (
     <div style={{ height: "500px", margin:"50px" }}>
+
       <MapContainer
       center={[49.1951, 16.6068]}
       zoom={13}
